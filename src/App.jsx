a7f118707d834a7f47875e701470cef718d9971d
anhdt <dtanhch116@gmail.com>
2025-05-18 16:05:00 +0700

    mixin trong scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import MainLayout from '@components/Layout/Layout';
+import Header from '@components/Header/Header';
+import Footer from '@components/Footer/Footer';
+import MyButton from '@components/Button/Button';
 
 function App() {
 
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <MainLayout>
+      <MyButton />
+    </MainLayout>
+    </>
   )
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+    return <div>
+        my footer
+    </div>;
+}
+
+export default Footer;
