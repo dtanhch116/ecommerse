f93017da065f2e4e866ff4a02d387982d23eae36
Dinh Tuan Anh <doran@MacBook-Air-cua-Tuan-Anh.local>
2025-05-26 19:59:39 +0700

    bug

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import Banner from "@components/Banner/Banner";
 import Header from "@components/Header/Header";
 import styles from "./styles.module.scss";
+import AdvanceHeading from "@components/AdvanceHeading/AdvanceHeading";
+import Info from "@components/Info/Info";
 
 function HomePage() {
     const { container } = styles;
@@ -9,6 +11,8 @@ function HomePage() {
             <div className={container}> 
                 <Header />
                 <Banner/>
+                <Info/>
+                <AdvanceHeading/>
             </div>
         </div>
      );
--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -6,23 +6,21 @@ import styles from "./styles.module.scss";
 function Info() {
     const { container } = styles;
     return (
-        <>
-            <MainLayout>
-                <div className={container}>
-                    {
-                        dataInfo.map((item, index) => {
-                            return (
-                                <div key={index}>
-                                    <InfoCart content={item.title} 
-                                    description={item.description} 
-                                    src={item.src}/>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-            </MainLayout>
-        </>
+        <MainLayout>
+            <div className={container}>
+                {
+                    dataInfo.map((item, index) => {
+                        return (
+                            <div key={index}>
+                                <InfoCart content={item.title}
+                                    description={item.description}
+                                    src={item.src} />
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </MainLayout>
     );
 }
 
