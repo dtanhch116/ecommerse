365787da47b7d10f7e0d95a65ea2a083449b967e
Dinh Tuan Anh <dtanhch116@gmail.com>
2025-06-11 06:11:55 +0700

    update sideBar

--- src/components/ContentsSideBar/Compare/Compare.jsx
+++ src/components/ContentsSideBar/Compare/Compare.jsx
@@ -1,6 +1,19 @@
+import HeaderSideBar from "@components/ContentsSideBar/Components/HeaderSideBar/HeaderSideBar";
+import { TfiReload } from "react-icons/tfi";
+import { CiHeart } from "react-icons/ci";
+import { PiShoppingCartThin } from "react-icons/pi";
+import styles from './styles.module.scss';
+import ItemProduct from "@components/ContentsSideBar/Components/ItemProduct/ItemProduct";
+import MyButton from "@components/Button/Button";
+
 function Compare() {
+    const { container } = styles;
     return (  
-        <div>compare</div>
+        <div className={container}>
+            <HeaderSideBar icon={<TfiReload style={{ fontSize: '30px' }}/>} title={'COMPARE'}/>
+            <ItemProduct />
+            <MyButton content={'VIEW COMPARE'}/>
+        </div>
     );
 }
 
---
+++ src/components/ContentsSideBar/Components/ItemProduct/ItemProduct.jsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.scss';
+import { IoCloseOutline } from "react-icons/io5";
+
+
+function ItemProduct() {
+    const { container, boxContent, title, price, boxClose } = styles;
+    return ( 
+        <div className={container}>
+            <img src="https://xstore.8theme.com/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-2.1-min.jpg" alt="" />
+            <div className={boxClose}>
+                <IoCloseOutline  style={{
+                    fontSize: '20px'
+                }}/>
+            </div>
+            <div className={boxContent}>
+                <div className={title}>title of product</div>
+                <div className={price}>$99.99</div>
+            </div>
+        </div>
+     );
+}
+
+export default ItemProduct;
